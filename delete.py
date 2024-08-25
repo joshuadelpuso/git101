8a8e86d4b3e39373db09403daf2a9f104a89155f
joshuadelpuso <joshuadelpuso@gmail.com>
2024-08-25 21:49:18 +0800

    Delete fields

---
+++ delete.py
@@ -0,0 +1,10 @@
+git clone: Creates a copy of an entire remote repository on your local machine. It’s like downloading the project so you can work on it locally.
+
+git push: Sends your changes from your local repository to a remote repository, making your updates available to others.
+
+
+git add: Prepares changes in your files to be saved. It tells Git which changes you want to include in the next commit.
+
+git commit: Saves the prepared changes to your local repository. It creates a snapshot of your project at that point in time.
+
+git push: Sends your saved changes from your local repository to a remote repository (like GitHub), making them available to others.
